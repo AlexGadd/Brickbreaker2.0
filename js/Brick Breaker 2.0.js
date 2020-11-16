e88ccf2299547fdb2c80f9279c1e34c7946866fc
AlexGadd <gadda023@gmail.com>
2020-11-16 16:26:29 +1300

    Realised I added a '.' rather than a '+' for 'brickInfo.height + brickInfo.padding' which meant it tried to load a resource which didn't exist

--- js/Brick Breaker 2.0.js
+++ js/Brick Breaker 2.0.js
@@ -64,8 +64,8 @@ function initBricks(){
     width: 50,
     height: 20,
     count: {
-      row: 3,
-      col: 7
+      row: 7,
+      col: 3
     },
     offset: {
       top: 50,
@@ -75,10 +75,10 @@ function initBricks(){
   };
   bricks = game.add.group();
   for(c=0; c<brickInfo.count.col; c++){
-    for(r=0; r<brickInfo.count.row;r++){
-      var brickX = (r*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
-      var brickY = (c*(brickInfo.height.brickInfo.padding))+brickInfo.offset.top;
-      newBrick = game.add.sprite(brickX,brickY, 'brick');
+    for(r=0; r<brickInfo.count.row; r++){
+      var brickX = (r*(brickInfo.width + brickInfo.padding))+brickInfo.offset.left;
+      var brickY = (c*(brickInfo.height + brickInfo.padding))+brickInfo.offset.top;
+      newBrick = game.add.sprite(brickX, brickY, 'brick');
       game.physics.enable(newBrick, Phaser.Physics.ARCADE);
       newBrick.body.immovable = true
       newBrick.anchor.set(0.5)
