cc4451083a0ee55f24aac8629b963c080092eaf5
AlexGadd <gadda023@gmail.com>
2020-11-16 15:04:34 +1300

    Added basic physics

--- js/Brick Breaker 2.0.js
+++ js/Brick Breaker 2.0.js
@@ -14,15 +14,18 @@ function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
-  game.stage.backgroundColor = '#FFFFFF';
+  game.stage.backgroundColor = '#eee';
   game.load.image('ball', 'img/ball.png');
     
 
 }
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
   ball = game.add.sprite(50,50,'ball');
+  game.physics.enable(ball,Phaser.Physics.ARCADE);
+  ball.body.velocity.set(150,150)
+  
 }
+
 function update() {
-  ball.x +=1
-  ball.y +=1
 }
